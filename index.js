5affbdb3d208d0a23cc91d27181155c8c124f673
jotkharoud <53591036+jotkharoud@users.noreply.github.com>
2020-03-13 00:33:39 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//action creator
+
+ export  const AddToList=(item)=>{         //item is the value passed is the text entered in input field
+       console.log(item)
+       return {
+           type:'Add_Value',
+
+
+           value:item,
+         }
+   }
